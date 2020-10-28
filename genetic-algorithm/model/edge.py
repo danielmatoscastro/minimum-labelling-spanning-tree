2e3c4342cf20dbdf688cf9f51c5156c67b141dc4
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-10-27 22:21:38 -0300

    adding __eq__ and __hash__ methods.

--- genetic-algorithm/model/edge.py
+++ genetic-algorithm/model/edge.py
@@ -1,3 +1,10 @@
 from collections import namedtuple
 
-Edge = namedtuple('Edge', ['u', 'v', 'l'])
+class Edge(namedtuple('Edge', ['u', 'v', 'l'])):
+    def __eq__(self, other: "Edge") -> bool:
+        nodes_are_equal = (self.u == other.u and self.v == other.v) or (self.v == other.u and self.u == other.v)
+        labels_are_equal = self.l == other.l
+        return nodes_are_equal and labels_are_equal
+
+    def __hash__(self) -> int:
+        return self.u ^ self.v ^ self.l
