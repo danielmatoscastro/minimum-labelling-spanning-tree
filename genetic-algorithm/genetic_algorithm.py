aaff8e960ea4eba1e8cfa17215ea8fe93cce3463
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-10-27 22:21:00 -0300

    adding elitism and crossover operators.

--- genetic-algorithm/genetic_algorithm.py
+++ genetic-algorithm/genetic_algorithm.py
@@ -1,5 +1,6 @@
 import random
 from typing import List, Tuple
+from math import ceil
 from model.instance import Instance
 from model.edge import Edge
 
@@ -13,14 +14,16 @@ class GeneticAlgorithm:
         random.seed(seed)
 
     def run(self) -> List[Edge]:
+        elite_size = ceil(self._elitism_rate*self._population_size)
+        new_solutions_size = self._population_size - elite_size
         population = self._generate_initial_population()
         should_stop = False
         i = 1
         while not should_stop:
             evaluated_pop = self._evaluate_population(population)
-            selected_pop = self._selection_operator(evaluated_pop)
-            population_with_new_solutions = self._crossover_operator(selected_pop)
-            population = self._mutation_operator(population_with_new_solutions)
+            elite = self._elitism_operator(evaluated_pop, elite_size)
+            new_solutions = self._crossover_operator(evaluated_pop, new_solutions_size)
+            population = elite + self._mutation_operator(new_solutions)
             should_stop = self._stopping_criterion(i)
             i += 1
 
@@ -44,7 +47,7 @@ class GeneticAlgorithm:
 
         return expanded_edges
 
-    def _evaluate_population(self, population: List[List[Edge]]) -> List[Tuple[List[Edge], int]]:
+    def _evaluate_population(self, population: List[List[Edge]]) -> List[Tuple[List[Edge], int, float]]:
         result = []
 
         for solution in population:
@@ -52,18 +55,28 @@ class GeneticAlgorithm:
             fitness = len(set(labels))
             result.append((solution, fitness))
         
+        fitness_sum = sum([fitness for _, fitness in result])
+        result = [(solution, fitness, fitness/fitness_sum) for solution, fitness in result]
+
         return result
 
-    def _selection_operator(self, population: List[Tuple[List[Edge], int]]) -> List[List[Edge]]:
-        pass
+    def _elitism_operator(self, population: List[Tuple[List[Edge], int, float]], elite_size: int) -> List[List[Edge]]:
+        sorted_population = sorted(population, key=lambda x: x[1])
+        return [solution for solution, _, _ in sorted_population[0:elite_size]]
 
-    def _crossover_operator(self, population: List[List[Edge]]) -> List[List[Edge]]:
-        pass
+    def _crossover_operator(self, population: List[Tuple[List[Edge], int, float]], new_solutions_size: int) -> List[List[Edge]]:
+        new_solutions = []
+        probs = [relative_fitness for _, _, relative_fitness in population]
+
+        for i in range(new_solutions_size):
+            father_1, father_2 = [solution for solution, _, _ in random.choices(population, weights=probs, k=2)]
+            graph = set(father_1 + father_2)
+            root = random.choice(graph)
+            new_solutions.append(self._dfs_tree(root, []))
+
+        return new_solutions
 
     def _mutation_operator(self, population: List[List[Edge]]) -> List[List[Edge]]:
-        pass
-
-    def _select_solution(self, population: List[List[Edge]]) -> List[Edge]:
         pass
 
     def _stopping_criterion(self, iteration: int) -> bool:
