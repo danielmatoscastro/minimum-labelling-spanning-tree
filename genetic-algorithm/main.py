95e3c2ac5084c7e8378389911f6552e743dc0823
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-10-28 21:59:42 -0300

    adding CLI.

--- genetic-algorithm/main.py
+++ genetic-algorithm/main.py
@@ -1,12 +1,14 @@
+import click
 from model.instance import Instance
 from genetic_algorithm import GeneticAlgorithm
 
-if __name__ == '__main__':
-    seed = 1
-    population_size = 100
-    mutation_rate = 0.7
-    elitism_rate = 0.2
-
+@click.command()
+@click.option('--file', type=click.Path(exists=True), help='path to .col file')
+@click.option('--seed', type=click.FLOAT, help='rng seed')
+@click.option('--population-size', type=click.INT, help='number of simultaneous solutions')
+@click.option('--mutation-rate', type=click.FLOAT, help='probability of mutation for each solution')
+@click.option('--elitism-rate', type=click.FLOAT, help='percentage of best solutions to be preserved')
+def run(file, seed, population_size, mutation_rate, elitism_rate):
     instance = Instance.load('data/testFile_8_75_37.col')
     algorithm = GeneticAlgorithm(instance, 
                                 seed,
@@ -15,3 +17,7 @@ if __name__ == '__main__':
                                 elitism_rate)
     solution = algorithm.run()
     print(solution)
+
+if __name__ == '__main__':
+    run()
+
