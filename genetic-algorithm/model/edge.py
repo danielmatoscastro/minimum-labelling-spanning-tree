ac9d772ef884d286e2e5e0fcd0d63d520a1cecb1
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-10-29 22:52:29 -0300

    removing useless isinstance call.

--- genetic-algorithm/model/edge.py
+++ genetic-algorithm/model/edge.py
@@ -2,9 +2,6 @@ from collections import namedtuple
 
 class Edge(namedtuple('Edge', ['u', 'v', 'l'])):
     def __eq__(self, other: "Edge") -> bool:
-        if not isinstance(other, Edge):
-            return False
-            
         nodes_are_equal = (self.u == other.u and self.v == other.v) or (self.v == other.u and self.u == other.v)
         labels_are_equal = self.l == other.l
         return nodes_are_equal and labels_are_equal
