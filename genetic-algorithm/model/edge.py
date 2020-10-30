be6fef10628f9b1bcc709dcc617e3fdb545b9ddb
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-10-29 23:15:08 -0300

    optimizing __hash__.

--- genetic-algorithm/model/edge.py
+++ genetic-algorithm/model/edge.py
@@ -1,10 +1,13 @@
 from collections import namedtuple
 
 class Edge(namedtuple('Edge', ['u', 'v', 'l'])):
+    _hash = None
     def __eq__(self, other: "Edge") -> bool:
         nodes_are_equal = (self.u == other.u and self.v == other.v) or (self.v == other.u and self.u == other.v)
         labels_are_equal = self.l == other.l
         return nodes_are_equal and labels_are_equal
 
     def __hash__(self) -> int:
-        return self.u ^ self.v ^ self.l
+        if not self._hash:
+            self._hash = self.u ^ self.v ^ self.l
+        return self._hash
