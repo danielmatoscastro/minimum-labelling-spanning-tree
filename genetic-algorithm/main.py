aae031aa05a95a2684aaf4000b849e3269dac624
Daniel Matos de Castro <daniel.matos.castro@gmail.com>
2020-11-01 14:41:02 -0300

    adding code to save output in json file.

--- genetic-algorithm/main.py
+++ genetic-algorithm/main.py
@@ -1,5 +1,6 @@
 import click
-#import time
+import time
+import json
 from model.instance import Instance
 from genetic_algorithm import GeneticAlgorithm
 
@@ -9,18 +10,34 @@ from genetic_algorithm import GeneticAlgorithm
 @click.option('--population-size', type=click.INT, help='number of simultaneous solutions')
 @click.option('--mutation-rate', type=click.FLOAT, help='probability of mutation for each solution')
 @click.option('--elitism-rate', type=click.FLOAT, help='percentage of best solutions to be preserved')
-def run(file, seed, population_size, mutation_rate, elitism_rate):
+@click.option('--output-file', type=click.STRING, help='path for output json file')
+def run(file, seed, population_size, mutation_rate, elitism_rate, output_file):
     instance = Instance.load(file)
     algorithm = GeneticAlgorithm(instance, 
                                 seed,
                                 population_size, 
                                 mutation_rate, 
                                 elitism_rate)
-    #start = time.time()
+    start_time = time.time()
     solution = algorithm.run()
-    #end = time.time()
-    print(solution)
+    stop_time = time.time()
+    print(f'Solution fitness: {solution[1]}')
+    print(f'Execution time: {stop_time - start_time}')
 
+    output = {
+        'parameters': {
+            'instance': file,
+            'seed': seed,
+            'population-size': population_size,
+            'mutation-rate': mutation_rate,
+            'elitism-rate': elitism_rate
+        },
+        'solution-fitness': solution[1],
+        'execution-time': stop_time - start_time
+    }
+
+    with open(output_file, mode='w') as out_file:
+        out_file.write(json.dumps(output, indent=2))
 
 if __name__ == '__main__':
     run()
